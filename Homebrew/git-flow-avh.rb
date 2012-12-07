955c2bb1eabddb70aad525e8f4eb1311bd81bb9d
Griffin Smith <smith@visionlink.org>
2012-12-06 17:58:17 -0700

    Created a basic homebrew formula
    Created a basic homebrew formula for review before submitting as a pull
    request to the homebrew repository. This is almost entirely based on the
    homebrew formula for nvie/gitflow.

---
+++ Homebrew/git-flow-avh.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class GitFlowCompletionAvh < Formula
+  homepage 'https://github.com/petervanderdoes/git-flow-completion'
+  url 'https://github.com/petervanderdoes/git-flow-completion.git', :tag => '0.4.2'
+  version '0.4.2'
+
+  head 'https://github.com/petervanderdoes/git-flow-completion.git', :branch => 'develop'
+end
+
+class GitFlowAvh < Formula
+  homepage 'https://github.com/nvie/gitflow'
+
+  # Use the tag instead of the tarball to get submodules
+  url 'https://github.com/petervanderdoes/gitflow.git', :tag => '1.3.1'
+  version '1.3.1'
+
+  head 'https://github.com/petervanderdoes/gitflow.git', :branch => 'develop'
+
+  def install
+    system "make", "prefix=#{prefix}", "install"
+
+    GitFlowCompletionAvh.new('git-flow-completion-avh').brew do
+      (prefix+'etc/bash_completion.d').install "git-flow-completion.bash"
+      (share+'zsh/site-functions').install "git-flow-completion.zsh"
+    end
+  end
+end
